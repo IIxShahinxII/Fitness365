c7cf2e13db9b30349678775a78f1b496cc013f50
IIxShahinxII <jafarzadehshahin@gmail.com>
2020-08-12 15:40:38 +0000

    completed homepage and css

--- Fitness365/settings.py
+++ Fitness365/settings.py
@@ -152,4 +152,4 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
 MEDIA_URL = '/media/'
-MEDIA_URL = os.path.join(BASE_DIR, 'media/')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
