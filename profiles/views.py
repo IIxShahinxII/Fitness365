6cd865eeaadce6a2bb995cafa4df87c6e13c701a
IIxShahinxII <jafarzadehshahin@gmail.com>
2020-08-21 12:44:03 +0000

    allauth updated

--- profiles/views.py
+++ profiles/views.py
@@ -1,11 +1,12 @@
-from django.shortcuts import render
-
-# Create your views here.
+from django.shortcuts import render, get_object_or_404
+from .models import UserProfile
 
 
 def profile(request):
+    profile = get_object_or_404(UserProfile, user=request.user)
 
     template = 'profiles/profile.html'
-    context = {}
-
+    context = {
+        'profile': profile,
+    }
     return render(request, template, context)
